88ae2d5f762ead927d4b9b5e18586b165c8a372a
Karina Valens <karyvalens@yahoo.com>
2022-08-30 21:00:24 +0200

    typing until the end of the string and reload

--- typewriter.js
+++ typewriter.js
@@ -5,8 +5,10 @@ document.addEventListener("DOMContentLoaded", initLoop);
 let counter = -1;
 
 const string = document.querySelector("#typewriter").innerHTML;
-const lengthOfString = string.length;
+const lengthOfString = (string.length) - 1;
 document.querySelector("#typewriter").innerHTML = " ";
+
+
 
 
 function initLoop() {
@@ -18,13 +20,13 @@ function loop() {
     //console.log("loop func loaded");
     if (counter < lengthOfString) {
         counter++;
-        //console.log(counter);
         document.querySelector("#typewriter").innerHTML += string[counter];
-
         setTimeout(loop, 100);
-    } else if (counter == lengthOfString) {
-        counter = -1;
-        console.log("It's over");
+    } else {
+        location.reload();
     }
 
 }
+
+
+

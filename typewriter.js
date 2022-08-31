faa91fd20a61ed653ee14b074b0dd00e9c2a6089
Karina Valens <karyvalens@yahoo.com>
2022-08-31 23:30:56 +0200

    typing working

--- typewriter.js
+++ typewriter.js
@@ -7,8 +7,10 @@ let counter = -1;
 const string = document.querySelector("#typewriter").innerHTML;
 const lengthOfString = (string.length) - 1;
 document.querySelector("#typewriter").innerHTML = " ";
-
-
+//audios
+let spaces;
+const audioletter = new Audio('typekey1.mp3');
+const audiospace = new Audio('typespace.mp3');
 
 
 function initLoop() {
@@ -18,15 +20,17 @@ function initLoop() {
 
 function loop() {
     //console.log("loop func loaded");
+    spaces = string.indexOf(" ");
     if (counter < lengthOfString) {
         counter++;
         document.querySelector("#typewriter").innerHTML += string[counter];
+        audioletter.play();
         setTimeout(loop, 100);
+    } else if (string == spaces) {
+        audiospace.play();
     } else {
+
         location.reload();
     }
 
 }
-
-
-
